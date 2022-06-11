e4e9c47e6c07ff7c2d57b5ea79f4cfeaac0abc59
milanceandrejic <milanceandrejic02@gmail.com>
2022-06-11 16:35:16 +0200

    v0.4

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,13 @@ public class Main
     public static void main(String[] args)
     {
         //ArrayList<Akcija> akcije = (ArrayList<Akcija>) new Tehnicar().UcitajJSON("imenik.json");
-        Tehnicar t=new Tehnicar();
-        ArrayList<Tehnicar> th = (ArrayList<Tehnicar>) t.UcitajJSON("test");
+        //Tehnicar t=new Tehnicar();
+        //ArrayList<Tehnicar> th = (ArrayList<Tehnicar>) t.UcitajJSON("test");
+
+        Tehnicar.UpisiUJSON(new ArrayList<>(),"tehnicari.json");
+        Doktor.UpisiUJSON(new ArrayList<>(),"doktori.json");
+        Davalac.UpisiUJSON(new ArrayList<>(),"davaoci.json");
+        Akcija.UpisiUJSON(new ArrayList<>(),"akcije.json");
+        Izvestaj.UpisiUJSON(new ArrayList<>(),"izvestaji.json");
     }
 }
